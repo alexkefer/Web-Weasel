a24bf57e37116599f14156c00826e06c6755faf8
alexkefer <classifiedpcig@gmail.com>
2023-11-12 22:48:49 -0800

    Modified Keagans source code to simplify command structure and added few ideas to README

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
 # p2pWebSearch
 P2P Web Search - WWU Senior Project - Alex Kefer, Keagan Edwards, Ryan Martin, Khang Tran
-
+Modified by Alex Kefer Version 1.0.0
 Program can be run using the following command.
 
 ``
-go run . 127.0.0.1:8080
+go run . 
 ``
 
 To connect another instance, run the following command.
 
 ``
-go run . 127.0.0.1:8081 127.0.0.1:8080 
+go run . <ip addr>:<port>
 ``
+
+# To Do
+- [x] Implement a way to broadcast to all peers in the network
+- [x] Implement a way to send a message to a specific peer and wait for a response
+- [x] Implement a command scheme to allow for user input to be sent to the network
--- main.go
+++ main.go
@@ -6,14 +6,20 @@ import (
 	"os"
 )
 
+// This is the main function of the program
 func main() {
 
-	if len(os.Args) < 2 {
-		fmt.Println("error: first arg must be own ip")
+	if len(os.Args) > 2 {
+		fmt.Println("error: either 0 or 1 arguments expected")
 		return
 	}
-
-	myAddr, myAddrParseErr := net.ResolveTCPAddr("tcp", os.Args[1])
+	port, err := findOpenPort(8080, 8100)
+	if err != nil {
+		fmt.Println("error finding open port:", err)
+		return
+	}
+	address := GetLocalIPAddress() + port
+	myAddr, myAddrParseErr := net.ResolveTCPAddr("tcp", address)
 
 	if myAddrParseErr != nil {
 		fmt.Println("error parsing my ip arg:", myAddrParseErr)
@@ -26,8 +32,8 @@ func main() {
 	addrChan <- myAddr
 
 	// If a second address is given, try to join its network
-	if len(os.Args) > 2 {
-		seedAddrArg := os.Args[2]
+	if len(os.Args) > 1 {
+		seedAddrArg := os.Args[1]
 
 		seedAddr, addrParseErr := net.ResolveTCPAddr("tcp", seedAddrArg)
 
@@ -41,6 +47,33 @@ func main() {
 
 	for {
 	}
+}
+
+// This function returns the local IP address of the machine
+// It is used to determine the address of the machine running the program so that other machines can connect to it
+func GetLocalIPAddress() string {
+	connection, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		panic("error getting local ip address")
+	}
+	defer connection.Close()
+	localAddr := connection.LocalAddr().(*net.UDPAddr)
+	return localAddr.IP.String()
+}
+
+// This function finds an open port in the range [startPort, endPort]
+func findOpenPort(startPort, endPort int) (string, error) {
+	for port := startPort; port <= endPort; port++ {
+		// Attempt to bind to this port
+		listener, listenerErr := net.Listen("tcp", fmt.Sprintf(":%d", port))
+		if listenerErr == nil {
+			// If we were able to bind, close the listener and return the port
+			listener.Close()
+			return fmt.Sprintf(":%d", port), nil
+		}
+	}
+	// If we were unable to bind to any ports, return an empty string
+	return "", fmt.Errorf("unable to find open port")
 }
 
 func SendAddMeRequest(addrChan chan<- net.Addr, from net.Addr, to net.Addr) {
