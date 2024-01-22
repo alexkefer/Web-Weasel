2ec1d2f88bf4b0564b7f4d46bd2f46aed19a18a9
kalternate <keaganmedwards@gmail.com>
2024-01-22 14:27:38 -0800

    Add placeholder api for file store and retrieve

--- p2psearch-backend/httpServer.go
+++ p2psearch-backend/httpServer.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"html"
 	"net/http"
+	"net/url"
 )
 
 func RunHttpServer(peerMap *PeerMap, shutdownChan chan<- bool) {
@@ -17,15 +18,23 @@ func RunHttpServer(peerMap *PeerMap, shutdownChan chan<- bool) {
 		peersHandler(w, r, peerMap)
 	})
 
+	http.HandleFunc("/store", func(w http.ResponseWriter, r *http.Request) {
+		storeFileHandler(w, r)
+	})
+
+	http.HandleFunc("/retrieve", func(w http.ResponseWriter, r *http.Request) {
+		retrieveFileHandler(w, r)
+	})
+
 	port, _ := findOpenPort(8080, 8180)
 
 	fmt.Printf("opening http server on port %s\n", port)
-	
+
 	http.ListenAndServe(port, nil)
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.String()))
 }
 
 func shutdownHandler(w http.ResponseWriter, r *http.Request, shutdownChan chan<- bool) {
@@ -39,4 +48,40 @@ func peersHandler(w http.ResponseWriter, r *http.Request, peerMap *PeerMap) {
 		fmt.Fprintf(w, "%s\n", html.EscapeString(key))
 	}
 	peerMap.mutex.RUnlock()
+}
+
+func storeFileHandler(w http.ResponseWriter, r *http.Request) {
+	path, err := getPathParam(r.URL)
+
+	if err != nil {
+		fmt.Fprintf(w, "Error storing file: %s", err)
+	} else {
+		// TODO
+		fmt.Fprintf(w, "Server is storing file found at %q", path)
+	}
+}
+
+func retrieveFileHandler(w http.ResponseWriter, r *http.Request) {
+	path, err := getPathParam(r.URL)
+
+	if err != nil {
+		fmt.Fprintf(w, "Error retrieving file: %s", err)
+	} else {
+		// TODO
+		fmt.Fprintf(w, "Server is retrieving file found at %q", path)
+	}
+}
+
+func getPathParam(fromUrl *url.URL) (string, error) {
+	params, err := url.ParseQuery(fromUrl.RawQuery)
+
+	if err != nil {
+		return "", err
+	}
+
+	if params.Has("path") {
+		return params.Get("path"), nil
+	}
+
+	return "", fmt.Errorf("request has no path param")
 }
